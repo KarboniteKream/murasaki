f620a2a8b056edcd49c601c78a68c2c0cb951f1d
Klemen Košir <klemen.kosir@kream.io>
2016-08-26 21:50:45 +0200

    Improve models and save the feed favicon

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // TODO: "use strict";
 // TODO: const, var or let?
-var Sequelize = require("sequelize"),
-	request = require("request"),
-	FeedParser = require("feedparser"),
-	config = require("./config");
+var Sequelize = require("sequelize");
+var request = require("request");
+var FeedParser = require("feedparser");
+var config = require("./config");
 
 var db = new Sequelize(config.database, config.username, config.password, {
 	host: config.host,
@@ -22,14 +22,25 @@ var db = new Sequelize(config.database, config.username, config.password, {
 });
 
 var Feed = db.define("feeds", {
-	name: {
+	title: {
 		type: Sequelize.STRING(255),
 	},
-	// TODO: Rename to link? Should this be the link to the feed or the website?
-	url: {
+	description: {
+		// TODO: Text?
 		type: Sequelize.STRING(1024),
 	},
-	// TODO: Description, icon, last updated.
+	website: {
+		type: Sequelize.STRING(1024),
+	},
+	link: {
+		type: Sequelize.STRING(1024),
+	},
+	icon: {
+		type: Sequelize.BLOB,
+	},
+	last_updated: {
+		type: Sequelize.DATE,
+	},
 });
 
 var Post = db.define("posts", {
@@ -39,8 +50,7 @@ var Post = db.define("posts", {
 	title: {
 		type: Sequelize.STRING(255),
 	},
-	// TODO: Rename to link?
-	url: {
+	link: {
 		type: Sequelize.STRING(1024),
 	},
 	author: {
@@ -69,84 +79,86 @@ if(url.startsWith("http") === false) {
 	url = "http://" + url;
 }
 
-var req = request(url);
+// Get the feed and pipe it to FeedParser.
+request(url).on("error", function(err) {
+	console.log("Unable to get the feed: ", err);
+}).on("response", function(response) {
+	// TODO: Test behaviour.
+	if(response.statusCode != 200) {
+		return this.emit("error", new Error("Bad status code."));
+	}
+
+	this.pipe(feedparser);
+});
+
 var feedparser = new FeedParser({
 	addmeta: false,
 });
 
-req.on("error", error);
-req.on("response", response);
+// An error occurred while parsing the feed.
+feedparser.on("error", function(err) {
+	console.log("Unable to parse the feed: ", err);
+});
 
-feedparser.on("error", error);
-feedparser.on("meta", meta);
-feedparser.on("readable", readable);
-feedparser.on("end", end);
-
-function error(err) {
-	console.log(err);
-}
-
-function response(res) {
-	var stream = this;
-
-	if(res.statusCode != 200) {
-		return this.emit("error", new Error("Bad status code."));
-	}
-
-	stream.pipe(feedparser);
-}
-
-function meta(meta) {
+// Get feed metadata.
+feedparser.on("meta", function(meta) {
 	feed.meta = {
 		title: meta.title,
 		description: meta.description,
-		link: meta.link,
-		date: meta.date, // Latest update?
-		pubdate: meta.pubdate,
-		favicon: meta.favicon, // Atom only.
+		website: meta.link,
+		link: meta.xmlurl,
+		// TODO: Icon.
+		icon: null,
+		last_updated: meta.date, // Latest update.
 	};
-}
+});
 
-function readable() {
+// Get feed posts.
+feedparser.on("readable", function() {
 	var item;
 
 	while((item = this.read()) !== null) {
 		feed.posts.push({
-			guid: item.guid,
 			title: item.title,
-			summary: item.summary,
+			link: item.link,
 			description: item.description,
 			pubdate: item.pubdate,
 			date: item.date,
-			link: item.link,
 			author: item.author,
 		});
 	}
-}
+});
 
-function end() {
-	// TODO: If feed.meta.favicon == null.
-	var icon = request("https://www.google.com/s2/favicons?domain=" + feed.meta.link);
-
-	Feed.create({
-		title: feed.meta.title,
-		url: feed.meta.link, // TODO: feed.meta.xmlUrl?
-	}).then(function(result) {
-		// TODO: created_at and updated_at are converted to UTC on save, but not on get.
-		var feed_id = result.id;
-
-		// TODO: When adding, check if the link already exists. Maybe GUID?
-		for(var i = 0; i < feed.posts.length; i++) {
-			var post = feed.posts[i];
-
-			Post.create({
-				feed_id: feed_id,
-				title: post.title,
-				url: post.link,
-				author: post.author,
-				date: post.pubdate, // Already in UTC.
-				content: post.description, // TODO: Summary?
-			});
+// Save the feed and its posts.
+feedparser.on("end", function() {
+	// Get the favicon.
+	request("https://www.google.com/s2/favicons?domain=" + feed.meta.website, function(err, res, body) {
+		if(err !== null) {
+			// TODO
 		}
+
+		if(res.statusCode == 200) {
+			feed.meta.icon = body;
+		}
+
+		Feed.create(feed.meta).then(function(result) {
+			// TODO: created_at and updated_at are converted to UTC on save, but not on get.
+			var feed_id = result.id;
+
+			// TODO: Sort articles by date. They could be unordered and also there will be order in the database.
+			// TODO: When adding, check if the link already exists.
+			for(var i = 0; i < feed.posts.length; i++) {
+				var post = feed.posts[i];
+
+				Post.create({
+					feed_id: feed_id,
+					title: post.title,
+					link: post.link,
+					author: post.author,
+					date: post.pubdate, // Already in UTC.
+					content: post.description, // TODO: Summary?
+				});
+			}
+		});
 	});
-}
+});
